f05bca5cb90a2bc26a97debe2c5778ca5e8006a6
Simon <siekie41@googlemail.com>
2022-02-23 22:43:57 +0100

    Batch Datei um shader zu compilieren aktualisiert.

--- VulkanPrototype/runShaderCompiler.bat
+++ VulkanPrototype/runShaderCompiler.bat
@@ -1,3 +1,3 @@
-C:\_projects\Libraries\Vulkan\Bin\glslangValidator.exe -V shader.vert
-C:\_projects\Libraries\Vulkan\Bin\glslangValidator.exe -V shader.frag
+C:\VulkanSDK\1.3.204.0\Bin\glslangValidator.exe -V shader.vert
+C:\VulkanSDK\1.3.204.0\Bin\glslangValidator.exe -V shader.frag
 pause
