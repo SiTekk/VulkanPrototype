86ecb5a63e91f09b42e40743fcb1e84219d2dd49
Simon <siekie41@googlemail.com>
2023-02-05 18:57:33 +0100

    Extracted renderer structs in separate unit.

---
+++ VulkanPrototype/src/Renderer/RendererUtils.cpp
@@ -0,0 +1,42 @@
+#include "RendererUtils.h"
+
+namespace VulkanPrototype::Renderer
+{
+    /*
+     * Member Functions
+     */
+
+    std::array<VkVertexInputAttributeDescription, 3> Vertex::getAttributeDescriptions()
+    {
+        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(Vertex, textureCoordinate);
+
+        return attributeDescriptions;
+    }
+
+    VkVertexInputBindingDescription Vertex::getBindingDescription()
+    {
+        VkVertexInputBindingDescription bindingDescription =
+        {
+            .binding = 0,
+            .stride = sizeof(Vertex),
+            .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+        };
+
+        return bindingDescription;
+    }
+}
---
+++ VulkanPrototype/src/Renderer/RendererUtils.h
@@ -0,0 +1,63 @@
+#ifndef RENDERERUTILS_H
+#define RENDERERUTILS_H
+
+#include <array>
+#include <fstream>
+#include <optional>
+#include <vector>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#include <glm/glm.hpp>
+#include <vulkan/vulkan.h>
+
+namespace VulkanPrototype::Renderer
+{
+    struct QueueFamily
+    {
+        std::optional<uint32_t> index;
+        uint32_t queueCount;
+    };
+
+    struct SurfaceDetails
+    {
+        VkSurfaceCapabilitiesKHR capabilities = {};
+        std::vector<VkSurfaceFormatKHR> formats;
+        std::vector<VkPresentModeKHR> presentModes;
+    };
+
+    struct UniformBufferObject
+    {
+        alignas(16) glm::mat4 model;
+        alignas(16) glm::mat4 view;
+        alignas(16) glm::mat4 proj;
+    };
+
+    struct Vertex
+    {
+        glm::vec2 pos;
+        glm::vec3 color;
+        glm::vec2 textureCoordinate;
+
+        static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
+        static VkVertexInputBindingDescription getBindingDescription();
+    };
+
+    struct UBOValues
+    {
+        //Model
+        float angle;
+        glm::vec3 axis;
+
+        //View
+        glm::vec3 eye;
+        glm::vec3 center;
+
+        //Proj
+        float fovy;
+        float near;
+        float far;
+    };
+}
+
+#endif // RENDERERUTILS_H
